7b2a0a6e3e9044bdac15766023884531d8a61f97
Daria Bodyakova <dashabodiakova0308@gmail.com>
2020-10-01 19:46:20 +0500

    Лабораторная № 5

---
+++ Laboratory_5/libs/employee.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+class Employee:
+    def __init__(self, name, seniority):
+        self.name = name
+        self.seniority = seniority
+        
+        self.grade = 1
+    
+    def grade_up(self):
+        """Повышает уровень сотрудника"""
+        self.grade += 1
+    
+    def publish_grade(self):
+        """Публикация результатов аккредитации сотрудников"""
+        print(self.name, self.grade, self.seniority)
+    
+    def check_if_it_is_time_for_upgrade(self):
+        pass
